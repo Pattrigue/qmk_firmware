45b374225a3a33fb352329908e41a6c57490edaf
Pattrigue <patrick@gamehouse.dk>
2021-05-06 21:07:32 +0200

    Update pattrigue_danish.c

--- keyboards/moonlander/keymaps/pattrigue/pattrigue_danish.c
+++ keyboards/moonlander/keymaps/pattrigue/pattrigue_danish.c
@@ -57,15 +57,15 @@ void reset_danish_mod_taps(void) {
 bool process_record_user_danish(uint16_t keycode, keyrecord_t *record) {
     if (layer_state != 0) return true;
 
-    if (keycode == TOGGLE_DANISH_MOD_TAP_KEYCODE) {
-        toggle_danish_mod_tap();        
+    if (keycode == TOGGLE_DANISH_MOD_TAP_KEYCODE && record->event.pressed) {
+        toggle_danish_mod_tap();    
     }
+
+    if (!danish_mod_tap_enabled) return false;
 
     if (record->event.pressed) {
 		reset_danish_mod_taps();
 	}
-
-    if (!danish_mod_tap_enabled) return true;
 
     switch (keycode) {
 		case KC_A:
